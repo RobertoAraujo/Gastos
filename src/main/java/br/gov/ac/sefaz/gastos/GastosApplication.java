2e133b908c12a74ade155f160046c2e0c2c6fc73
Roberto Moraes <robertomoraes@lampp-it.com.br>
2022-02-19 15:06:46 -0500

    primeiro commit

---
+++ src/main/java/br/gov/ac/sefaz/gastos/GastosApplication.java
@@ -0,0 +1,13 @@
+package br.gov.ac.sefaz.gastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/ac/sefaz/gastos/GastosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.ac.sefaz.gastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
